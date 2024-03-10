44f8515e04d5f9a923082d4bc79d3d5dbf752f93
PhamBaAnh08 <143930351+PhamBaAnh08@users.noreply.github.com>
2024-03-10 17:14:53 +0700

    Update Aimbot.lua

--- Blox Fruits/Aimbot.lua
+++ Blox Fruits/Aimbot.lua
@@ -1,10 +1,9 @@
 local SexyAimbot = nil
-local Mouse = game.Players.LocalPlayer:GetMouse()
 SexyAimbot = hookmetamethod(game, "__index", newcclosure(function(self, Index)
-	if self == Mouse and not checkcaller() then
-		if Index == "Hit" or Index == "hit" then
-		    return _G.CFrameTar or LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
-		end
-	end
-	return SexyAimbot(self, Index)
+            if self == Mouse and not checkcaller() then
+                if Index == "Hit" or Index == "hit" then
+                    return CFrameTar or LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame
+                end
+            end
+            return SexyAimbot(self, Index)
 end))
